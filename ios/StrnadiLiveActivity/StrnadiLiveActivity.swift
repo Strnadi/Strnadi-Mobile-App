f97c5d9bcc2f1ee97385513cc8538a0218d39723
Jan Drobílek <drobilek.jan@gmail.com>
2025-03-29 20:23:32 +0100

    recording live activity

---
+++ ios/StrnadiLiveActivity/StrnadiLiveActivity.swift
@@ -0,0 +1,135 @@
+//
+//  StrnadiLiveActivity.swift
+//  StrnadiLiveActivity
+//
+//  Created by Jan Drobílek on 29/3/25.
+//
+
+import WidgetKit
+import SwiftUI
+import ActivityKit
+
+struct Provider: AppIntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+    }
+
+    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: configuration)
+    }
+    
+    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        return Timeline(entries: entries, policy: .atEnd)
+    }
+
+//    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationAppIntent
+}
+
+struct StrnadiLiveActivityEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Favorite Emoji:")
+            Text(entry.configuration.favoriteEmoji)
+        }
+    }
+}
+
+struct StrnadiLiveActivity: Widget {
+    let kind: String = "StrnadiLiveActivity"
+
+    var body: some WidgetConfiguration {
+        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+            StrnadiLiveActivityEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
+        }
+    }
+}
+
+extension ConfigurationAppIntent {
+    fileprivate static var smiley: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "😀"
+        return intent
+    }
+    
+    fileprivate static var starEyes: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "🤩"
+        return intent
+    }
+}
+
+struct LiveActivitiesAppAttributes: ActivityAttributes, Identifiable {
+  public typealias LiveDeliveryData = ContentState // don't forget to add this line, otherwise, live activity will not display it.
+
+  public struct ContentState: Codable, Hashable { }
+
+  var id = UUID()
+}
+
+extension LiveActivitiesAppAttributes {
+  func prefixedKey(_ key: String) -> String {
+    return "\(id)_\(key)"
+  }
+}
+
+// Create shared default with custom group
+let sharedDefault = UserDefaults(suiteName: "group.delta.strnadi")!
+
+struct RecordingActivity: Widget {
+  var body: some WidgetConfiguration {
+    ActivityConfiguration(for: LiveActivitiesAppAttributes.self) { context in
+      // Main view for the live activity
+      let testText = sharedDefault.string(forKey: context.attributes.prefixedKey("testText"))!
+      Text("Live Activity: \(myVariableFromFlutter)")
+    } dynamicIsland: { context in
+      DynamicIsland {
+        // Expanded regions for the Dynamic Island
+        DynamicIslandExpandedRegion(.leading) {
+          Text("Leading")
+        }
+        DynamicIslandExpandedRegion(.trailing) {
+          Text("Trailing")
+        }
+        DynamicIslandExpandedRegion(.bottom) {
+          Text("Bottom")
+        }
+      } compactLeading: {
+        Text("CL")
+      } compactTrailing: {
+        Text("CT")
+      } minimal: {
+        Text("Min")
+      }
+    }
+  }
+}
+
+#Preview(as: .systemSmall) {
+    StrnadiLiveActivity()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
---
+++ ios/StrnadiLiveActivity/StrnadiLiveActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  StrnadiLiveActivityBundle.swift
+//  StrnadiLiveActivity
+//
+//  Created by Jan Drobílek on 29/3/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StrnadiLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        StrnadiLiveActivity()
+        StrnadiLiveActivityControl()
+        StrnadiLiveActivityLiveActivity()
+    }
+}
