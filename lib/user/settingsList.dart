6ac9881b67aebcbeea90b43347fecb9fa600de9a
Jan Drobílek <drobilek.jan@gmail.com>
2025-03-31 22:47:49 +0200

    implemented md renderer and about

--- lib/user/settingsList.dart
+++ lib/user/settingsList.dart
@@ -17,6 +17,8 @@ import 'package:flutter/material.dart';
 import 'package:strnadi/user/settingsPages/appSettings.dart';
 import 'package:strnadi/user/settingsPages/userInfo.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:strnadi/md_renderer.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class MenuScreen extends StatelessWidget {
   final List<String> menuItems = [
@@ -30,10 +32,7 @@ class MenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery
-        .of(context)
-        .size
-        .height * 0.5;
+    final height = MediaQuery.of(context).size.height * 0.5;
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -69,24 +68,48 @@ class MenuScreen extends StatelessWidget {
     if (index == 0) {
       Navigator.push(
           context, MaterialPageRoute(builder: (context) => ProfileEditPage()));
-    }
-    else if (index == 1) {
+    } else if (index == 1) {
       _showMessage("Nastavení ještě není dostupné", context);
       //Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
-    }
-    else if (index == 3) {
-      _launchURL('https://new.strnadi.cz/how-to-record');
-    }
-    else if (index == 4) {
-      _launchURL("https://new.strnadi.cz/about-project");
-    }
-    else {
+    } else if (index == 3) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  MDRender(mdPath: 'assets/docs/how-to-record.md')));
+    } else if (index == 4) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  MDRender(mdPath: 'assets/docs/about-project.md')));
+    } else if (index == 5) {
+      _showAboutDialog(context);
+    } else {
       _showMessage("tato funkce neni jeste dostupna", context);
     }
   }
 
   void _showMessage(String s, BuildContext context) {
     showDialog(
-        context: context, builder: (context) => AlertDialog(title: Text(s)));
+      context: context,
+      builder: (context) => AlertDialog(title: Text(s)),
+    );
+  }
+
+  void _showAboutDialog(BuildContext context) async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    showAboutDialog(
+      context: context,
+      applicationName: 'Strnadi Mobile App',
+      applicationVersion: packageInfo.version,
+      applicationIcon: Icon(Icons.info_outline),
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 15),
+          child: Text('Creators: Marian Pecqueur && Jan Drobílek'),
+        ),
+      ],
+    );
   }
 }
