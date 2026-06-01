2b0846ca8ed1ec08de3c5e2973ac4378ec569e67
Jan Drobílek <drobja@delta-studenti.cz>
2026-06-01 12:19:43 +0200

    Remove redundant email test import

--- test/utils/email_validator_test.dart
+++ test/utils/email_validator_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:strnadi/auth/email_input_formatter.dart';
 import 'package:strnadi/auth/email_validator.dart';
